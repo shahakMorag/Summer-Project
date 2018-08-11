d41d3d6dae02606fbd613bf0b87b68b8d72232d4
shahakMorag <shahak.morag@gmail.com>
2018-08-11 15:28:39 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	test.py

--- test.py
+++ test.py
@@ -1,3 +1,44 @@
-from PIL import Image
-im = Image.open("test.png")
+from PIL import Image, ImageFilter
+import numpy as np
+import random
+
+im1 = Image.open("test.png")
 im2 = Image.open("test2.jpg")
+
+size = 500, 500
+
+background = Image.new("RGBA", im1.size, (255, 255, 255))
+background.paste(im1)
+im1 = background
+
+background = Image.new("RGBA", im2.size, (255, 255, 255))
+background.paste(im2)
+im2 = background
+
+im1 = im1.resize(size, Image.ANTIALIAS)
+im2 = im2.resize(size, Image.ANTIALIAS)
+
+#Image.alpha_composite(im1, im2).show()
+
+#Image.blend(im1, im2, 0.3).show()
+
+#Image.eval(im2, lambda x: 3 * x % 256).show()
+
+#im1.convert(mode="L").show()
+
+#im1.convert(mode="1").show()
+
+#Image.fromarray(np.random.randn(1000,1000), mode='RGBA').show()
+
+#im2.crop((20, 20, 80, 80)).show()
+
+#im1.filter(ImageFilter.CONTOUR).show()
+
+#im1.filter(ImageFilter.FIND_EDGES).show()
+
+#im1.filter(ImageFilter.EDGE_ENHANCE_MORE).show() #important
+
+#print(im1.getbbox()) #output (0, 0, 500, 500)
+
+
+
