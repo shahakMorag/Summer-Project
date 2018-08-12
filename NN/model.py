952fa7d66401ef6dee6a080989d5fa3020861174
eitanK <eitan.kosman@gmail.com>
2018-08-12 23:25:04 +0300

    change net

--- NN/model.py
+++ NN/model.py
@@ -49,4 +49,4 @@ def custom_network():
     NN = fully_connected(NN, 5, activation='softmax')
 
     NN = regression(NN, optimizer='adam', learning_rate=0.01, loss='categorical_crossentropy', name='targets')
-    return tflearn.DNN(NN, tensorboard_verbose=3)
+    return tflearn.DNN(NN, tensorboard_dir="../logs/", tensorboard_verbose=0)
