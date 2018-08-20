f6eb2ebcff13f5d806b2d78b20b489e3f77e7b60
eitan.k <ihavegalaxyS9>
2018-08-20 13:12:24 +0300

    created models

--- NN/train_existing_NN.py
+++ NN/train_existing_NN.py
@@ -12,50 +12,20 @@ epochs = 3
 
 
 def train(start, limit, model, trained_models_path, path_to_data, patience=30):
-    '''
-    x_train, y_train = make_inputs(start, limit, num_classes)
-
-    x_train = x_train.astype('float32')
-    x_train /= 255
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-
-    #callbacks
-    #csv_logger = CSVLogger(log_file_path, append=False)
-    early_stop = EarlyStopping('acc', patience=patience)
-    reduce_lr = ReduceLROnPlateau('acc', factor=0.1,
-                                  patience=int(patience / 4), verbose=1)
-    model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
-    # model_checkpoint = ModelCheckpoint(model_names, 'acc', verbose=1, save_best_only=True, period=40)
-
-    callbacks = [early_stop, reduce_lr]
-
-    print('Starting to fit the model...')
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              verbose=1,
-              callbacks= callbacks,
-              validation_split=0.0125)
-    '''
     # callbacks
-    # csv_logger = CSVLogger(log_file_path, append=False)
     early_stop = EarlyStopping('acc', patience=patience)
     reduce_lr = ReduceLROnPlateau('acc', factor=0.1,
                                   patience=int(patience / 4), verbose=1)
-    model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
-    # model_checkpoint = ModelCheckpoint(model_names, 'acc', verbose=1, save_best_only=True, period=40)
-
     callbacks = [early_stop, reduce_lr]
 
     print('Starting to fit the model...')
 
-    train_datagen = ImageDataGenerator(
+    train_data_gen = ImageDataGenerator(
         validation_split=0.0125,
         rescale=1. / 255,
     )
 
-    train_generator = train_datagen.flow_from_directory(
+    train_generator = train_data_gen.flow_from_directory(
         path_to_data,
         target_size=(128, 128),
         batch_size=batch_size,
