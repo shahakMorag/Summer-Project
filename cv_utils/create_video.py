84f0791f97aa0f559bc87bbb30505e66d909a177
eitan.k <ihavegalaxyS9>
2018-09-24 15:52:00 +0300

    finished project

---
+++ cv_utils/create_video.py
@@ -0,0 +1,47 @@
+from os import path
+
+import cv2
+
+from cv_utils.enc_dec_create_image import create_image
+
+
+def segment_movie(movie_path):
+    images = []
+    vidcap = cv2.VideoCapture(movie_path)
+    success, image = vidcap.read()
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    height, width, _ = image.shape
+    video = cv2.VideoWriter(r'D:/video.mp4', fourcc, 30.0, (width, height))
+    print(image.shape)
+    count = 0
+    while success:
+        images.append(cv2.resize(image, None, fx=2, fy=2))
+        if len(images) == 60:
+            print("image number:", count)
+            res = create_image("../models/encoder_decoder/semantic_seg_2018_09_21_7_51.model", images, 60)
+            for image, i in zip(res, range(len(res))):
+                tmp_img = images[i]
+                h, w = tmp_img.shape[:2]
+                tmp_img = tmp_img[:h - (h % 372), :w - (w % 372)]
+                video.write(cv2.resize(cv2.addWeighted(tmp_img, 0.3,
+                                            cv2.resize(image, (tmp_img.shape[1], tmp_img.shape[0])), 0.7, 0), (width, height)))
+            images = []
+        success, image = vidcap.read()
+        print('Read a new frame: ', success)
+        count += 1
+
+    if len(images) > 0:
+        res = create_image("../models/encoder_decoder/semantic_seg_2018_09_21_7_51.model", images)
+        for image, i in zip(res, range(len(res))):
+            tmp_img = images[i]
+            h, w = tmp_img.shape[:2]
+            tmp_img = tmp_img[:h - (h % 372), :w - (w % 372)]
+            video.write(cv2.resize(cv2.addWeighted(tmp_img, 0.3,
+                                                   cv2.resize(image, (tmp_img.shape[1], tmp_img.shape[0])), 0.7, 0),
+                                   (width, height)))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+
+segment_movie(r'D:\test.mp4')
--- cv_utils/enc_dec_create_image.py
+++ cv_utils/enc_dec_create_image.py
@@ -1,47 +1,89 @@
+import glob
+from os import path
+
 import cv2
 import keras
 import numpy as np
 from keras.models import load_model
 from keras_preprocessing.image import ImageDataGenerator
 from crop_utils import create_crops, calc_dim, keys2img
+from tqdm import tqdm
+import os
 
 
-def create_image(model_path, images):
+def create_image(model_path, images, batch_size=1):
     output_size = 24
 
     crops = [create_crops(image, 372, 372, 372 // 2, 372 // 2) for image in images]
+    # cv2.imshow("", crops[0])
+    # cv2.waitKey(10000)
     crops = np.array(crops).reshape((-1, 372, 372, 3))
     height, width = calc_dim(images[0], 372, 372, 372 // 2, 372 // 2)
     target = np.zeros((len(images), height * output_size, width * output_size, 3), dtype=np.uint8)
     model = load_model(model_path)
 
-    test_generator = ImageDataGenerator().flow(x=np.array(crops), batch_size=1, shuffle=False)
+    test_generator = ImageDataGenerator().flow(x=np.array(crops), batch_size=batch_size, shuffle=False)
 
     predicts = model.predict_generator(test_generator,
-                                       steps=len(crops),
+                                       steps=len(crops) // batch_size,
                                        verbose=1,
                                        workers=8,
                                        use_multiprocessing=False)
 
-    predicts = np.array(predicts).reshape((-1, output_size * output_size, 5))
-    predicts = predicts.argmax(axis=-1).reshape((-1,))
-    predicts = keys2img(predicts, output_size, output_size, height * width)
-    predicts = predicts.reshape((height, width, output_size, output_size, 3))
+    predicts = np.array(predicts).reshape((len(images), -1, output_size * output_size, 5))
+    predicts = predicts.argmax(axis=-1).reshape((len(images), -1))
+    predicts = np.array([keys2img(predict, output_size, output_size, height * width) for predict in predicts])
+    predicts = predicts.reshape((len(images), height, width, output_size, output_size, 3))
 
     for image_index in range(len(images)):
         for y in range(height):
             for x in range(width):
-                target[image_index, y * output_size:(y + 1) * output_size, x * output_size:(x + 1) * output_size] = predicts[y][x]
+                target[image_index, y * output_size:(y + 1) * output_size, x * output_size:(x + 1) * output_size] = \
+                predicts[image_index, y, x]
 
     return target
 
 
-image = cv2.imread("../test/image transformations/IMG_0781.JPG", 1)
-image1 = cv2.imread("../test/image transformations/IMG_0781.JPG", 1)
-cv2.imshow("t", cv2.resize(image, None, fx=0.2, fy=0.2))
-cv2.waitKey(2000)
-res = create_image("../models/encoder_decoder/semantic_seg_2018_09_21_7_51.model", [image])
-cv2.imwrite("C:\Tomato_Classification_Project\Tomato_Classification_Project\cropped_data/test/enc_dec_5562.png", res)
+results_dir = 'D:/results_encoder_decoder'
 
-cv2.imshow("fds", res[0])
-cv2.waitKey(10000)
+
+def check(image_path):
+    res = cv2.imread(image_path, 1)
+    if res is None:
+        print(image_path)
+
+    return res
+
+
+def create(src):
+    paths = [image_path for image_path in glob.iglob(path.join(src, "*.JPG"))]
+    print('There are', len(paths), 'paths')
+    n = 20
+    path_divides = [paths[i:min(i + n, len(paths))] for i in range(0, len(paths), n)]
+    for path_list in path_divides:
+        images = [check(image_path) for image_path in tqdm(path_list)]
+        res = create_image("../models/encoder_decoder/semantic_seg_2018_09_21_7_51.model", images)
+        for result_image, i in zip(res, range(len(res))):
+            tmp_path = path_list[i].replace("/", "\\")
+            name = "\\".join(tmp_path.split('\\')[-3:])
+            full_name = path.join(results_dir, name)
+            cv2.imwrite(full_name, result_image)
+
+
+if __name__ == '__main__':
+    main_dir = 'C:\Tomato_Classification_Project\Tomato_Classification_Project\Data\Demonstration_greenhouse_tomato_Hazera\Demonstration_greenhouse_tomato_Hazera'
+    for subdir in os.listdir(main_dir):
+        current_path = path.join(main_dir, subdir)
+        save_path = path.join(results_dir, subdir)
+
+        if not path.exists(save_path):
+            os.mkdir(save_path)
+
+        for subdir2 in os.listdir(current_path):
+            current_path2 = path.join(current_path, subdir2)
+            save_path2 = path.join(save_path, subdir2)
+
+            if not path.exists(save_path2):
+                os.mkdir(save_path2)
+
+            create(current_path2)
